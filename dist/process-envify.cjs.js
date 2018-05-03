232296b9dd30e5f83672c970feae664cc9dd49f5
Shyam-Chen <shyamchen1994@gmail.com>
2018-05-03 13:23:52 +0800

    3rd Commit

--- dist/process-envify.cjs.js
+++ dist/process-envify.cjs.js
@@ -1,6 +1,23 @@
 'use strict';
 
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var polyfill = function () {
+  var reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+  var isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+  var concat = Function.bind.call(Function.call, Array.prototype.concat);
+  var keys = Reflect.ownKeys;
+
+  if (!Object.values) {
+    Object.values = function values(O) {
+      return reduce(keys(O), function (v, k) { return concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []); }, []);
+    };
+  }
+};
+
 function index (obj) {
+  polyfill();
+
   var envify = {};
   var keys = Object.keys(obj);
   var values = Object.values(obj);
@@ -12,5 +29,6 @@ function index (obj) {
   return envify;
 }
 
-module.exports = index;
+exports.polyfill = polyfill;
+exports.default = index;
 //# sourceMappingURL=process-envify.cjs.js.map
--- dist/process-envify.esm.js
+++ dist/process-envify.esm.js
@@ -1,4 +1,19 @@
+var polyfill = function () {
+  var reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+  var isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+  var concat = Function.bind.call(Function.call, Array.prototype.concat);
+  var keys = Reflect.ownKeys;
+
+  if (!Object.values) {
+    Object.values = function values(O) {
+      return reduce(keys(O), function (v, k) { return concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []); }, []);
+    };
+  }
+};
+
 function index (obj) {
+  polyfill();
+
   var envify = {};
   var keys = Object.keys(obj);
   var values = Object.values(obj);
@@ -11,4 +26,5 @@ function index (obj) {
 }
 
 export default index;
+export { polyfill };
 //# sourceMappingURL=process-envify.esm.js.map
